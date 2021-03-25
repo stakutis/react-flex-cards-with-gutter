cc3663f00159943c4965f1703ffc5789ae49347b
stakutis <chris.stakutis@gmail.com>
2021-03-25 16:10:21 -0400

    Working

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import styled from "styled-components";
+
+const PageContainer = styled.div`
+  margin: 50px auto;
+  background: grey;
+  width: 80vw;
+  height: 800px;
+`;
+
+const ColumnsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  background: orange;
+`;
+
+const media = {
+  md: (styles) => `
+    @media only screen and (max-width: 600px) {
+      ${styles}
+    }
+  `,
+};
+
+const Gutter = styled.div`
+  flex: 1;
+  background: lightcoral;
+  text-align: center;
+  padding-top: 100px;
+  ${(props) =>
+    props.collapse &&
+    media[props.collapse](`
+    display:none;
+    `)};
+`;
+
+const CardContainer = styled.div`
+  margin-top: 50px;
+  display: inline-flex; // Not sure why but margin-top wont work otherwise
+  flex-wrap: wrap;
+  justify-content: flex-start; //center; //flex-start;
+  border: 2px solid blue;
+  flex: 5;
+`;
+
+const Card = styled.div`
+  width: 100px;
+  height: 100px;
+  background: pink;
+  margin: 5px;
+  overflow: hidden;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <PageContainer>
+      Some random stuff..
+      <ColumnsContainer>
+        <Gutter collapse="md">
+          Gutter; Vanishes at screen size 'md' and smaller.
+        </Gutter>
+        <CardContainer>
+          <Card>
+            This is my card and it is very long but that's fine with me because
+            I like to see if it will truncate or overlow or whatever happens
+            happens...
+          </Card>
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </CardContainer>
+        <Gutter collapse="md">Gutter</Gutter>
+      </ColumnsContainer>
+      The rest of the app/page/...
+    </PageContainer>
   );
 }
 
